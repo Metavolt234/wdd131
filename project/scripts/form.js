8c2e4667878d8dcf235c29605c937164480bc9c5
Metavolt234 <metavolt3@gmail.com>
2026-06-06 12:51:23 +0300

    form

---
+++ project/scripts/form.js
@@ -0,0 +1,33 @@
+const products = [
+    {
+        id: "fc-1888",
+        name: "Flux Capacitor"
+    },
+    {
+        id: "fc-2050",
+        name: "Power Laces"
+    },
+    {
+        id: "fs-1987",
+        name: "Flying Skateboard"
+    },
+    {
+        id: "ac-2000",
+        name: "Anti-Gravity Boots"
+    },
+    {
+        id: "jp-3000",
+        name: "Jet Pack"
+    }
+];
+
+const productSelect = document.querySelector("#product");
+
+products.forEach(product => {
+    const option = document.createElement("option");
+
+    option.value = product.id;
+    option.textContent = product.name;
+
+    productSelect.appendChild(option);
+});
---
+++ project/scripts/review.js
@@ -0,0 +1,9 @@
+let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+
+reviewCount++;
+
+localStorage.setItem("reviewCount", reviewCount);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("#reviewCount").textContent = reviewCount;
+});
