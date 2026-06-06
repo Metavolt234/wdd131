e618e0e8d43db326ea0823b25c38023c672a2372
Metavolt234 <metavolt3@gmail.com>
2026-06-06 13:14:15 +0300

    form

--- project/scripts/form.js
+++ project/scripts/form.js
@@ -1,23 +1,28 @@
 const products = [
     {
         id: "fc-1888",
-        name: "Flux Capacitor"
+        name: "Flux Capacitor",
+        avgRating: 4.5
     },
     {
         id: "fc-2050",
-        name: "Power Laces"
+        name: "Power Laces",
+        avgRating: 4.7
     },
     {
         id: "fs-1987",
-        name: "Flying Skateboard"
+        name: "Flying Skateboard",
+        avgRating: 4.3
     },
     {
         id: "ac-2000",
-        name: "Anti-Gravity Boots"
+        name: "Anti-Gravity Boots",
+        avgRating: 4.8
     },
     {
         id: "jp-3000",
-        name: "Jet Pack"
+        name: "Jet Pack",
+        avgRating: 4.2
     }
 ];
 
--- project/scripts/review.js
+++ project/scripts/review.js
@@ -1,9 +1,11 @@
-let reviewCount = Number(localStorage.getItem("reviewCount")) || 0;
+let reviewCount =
+    Number(localStorage.getItem("reviewCount")) || 0;
 
 reviewCount++;
 
 localStorage.setItem("reviewCount", reviewCount);
 
 document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("#reviewCount").textContent = reviewCount;
+    document.querySelector("#reviewCount").textContent =
+        reviewCount;
 });
